f9c0898cb3a8ac2ad439fdf3f9afb68764307768
a1816653@adelaide.edu.au <1245766137@qq.com>
2023-07-20 10:48:14 +0930

    Initial Commit

---
+++ IosApp/IosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosAppApp.swift
+//  IosApp
+//
+//  Created by Wu Zuxing on 20/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct IosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
